16b360ca147bd967ec57754edb0603f8783fc7c3
samveltorosyanpy <samveltorosyan90@gmail.com>
2022-08-23 16:23:23 +0400

    connection database postgres in app

--- con/classes/SQL/tables/conf/config.py
+++ con/classes/SQL/tables/conf/config.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-engine = create_engine("postgresql://postgres:postgres@localhost:5432/bot")
+engine = create_engine("postgres://urfelmrraawjmv:56fe7c20b0ca3768ec926f34a7a3ef47fed1e1e9d3029398fe3c640a148e1b50@ec2-52-49-120-150.eu-west-1.compute.amazonaws.com:5432/d1n5sd95rreaum")
 
 Session = sessionmaker(bind=engine)
 
