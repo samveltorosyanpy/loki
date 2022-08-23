1902e92e8d777f2a984b9e3dc4b4739ccca5ad9b
samveltorosyanpy <samveltorosyan90@gmail.com>
2022-08-23 16:50:02 +0400

    connection database postgres in app

--- con/classes/SQL/StartingPostgres.py
+++ con/classes/SQL/StartingPostgres.py
@@ -1,8 +1,8 @@
-from con.classes.SQL.tables.conf.config import Base, engine, Session
 from con.classes.SQL.tables.Users import TelegramUser
 from con.classes.SQL.tables.Transactions import TransactionExchange
 from con.classes.SQL.tables.TransactionPhotos import TransactionPhoto
 from con.classes.SQL.tables.TransactionIsNotPhoto import TransactionIsNotPhoto
+from con.classes.SQL.tables.conf.config import Base, engine, Session
 
 Base.metadata.create_all(engine)
 
--- con/classes/conf/configuration.py
+++ con/classes/conf/configuration.py
@@ -2,6 +2,9 @@ import os
 
 
 PATH = os.path.dirname(__file__)[:-17]
+
+SQLALCHEMY_DATABASE_URI = "postgres://urfelmrraawjmv:56fe7c20b0ca3768ec926f34a7a3ef47fed1e1e9d3029398fe3c640a148e1b50@ec2-52-49-120-150.eu-west-1.compute.amazonaws.com:5432/d1n5sd95rreaum"
+
 TOKEN = '5728675320:AAEnXeGckEN2yGpyI_XzDPhpVo-pLeqy9KQ'
 
 APP_URL = f"https://lokichangebot.herokuapp.com/{TOKEN}"
