2ed7f4fb3c4fa941cf14b496f848021eb8b76faf
Samvel <samveltorosyan90@gmail.com>
2022-08-25 14:58:32 +0400

    to delete for .env file in application

--- asyncio_example.py
+++ asyncio_example.py
@@ -1,51 +1,51 @@
-import telebot
-import schedule
-from time import sleep
-from datetime import datetime, timedelta
-from con.classes.conf.configuration import *
-from con.classes.SQL.StartingPostgres import *
-
-bot = telebot.TeleBot(TOKEN)
-def schedule_checker():
-    while True:
-        schedule.run_pending()
-        sleep(1)
-
-def waiting():
-    # await asyncio.sleep(5)
-    while True:
-        transaction_list = list(Sessions.query(TransactionExchange.transaction_id, TransactionExchange.user_id, TransactionExchange.datetime, TransactionExchange.state_transaction).filter(TransactionExchange.state_transaction == 'waiting_user_photo').group_by(TransactionExchange.transaction_id).order_by(TransactionExchange.transaction_id.desc()))
-        for row in transaction_list:
-            try:
-                datet = datetime.strptime(row[2].strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S") + timedelta(minutes=limit_time_send_photo)
-                if datet <= datetime.now():
-                    print("send message")
-
-                else:
-                    continue
-
-            except AttributeError as ex:
-                continue
-
-
-def bot_start(bot):
-
-    @bot.message_handler(commands=["start"])
-    def choose_transaction(message):
-        bot.send_message(message.chat.id, message.text)
-
-
-
-if __name__ == '__main__':
-    bot_start(bot)
-    schedule.every().saturday.at("18:28").do(waiting)
-    bot.polling(none_stop=True, interval=0)
-# main_loop = asyncio.get_event_loop()
-# cors = asyncio.wait([waiting(), bot_start(bot)])
-# main_loop.run_until_complete(cors)
-# main_loop.run_forever()
-
-
-
-
-# .strftime("%m/%d/%Y, %H:%M:%S")
+# import telebot
+# import schedule
+# from time import sleep
+# from datetime import datetime, timedelta
+# from con.classes.conf.configuration import *
+# from con.classes.SQL.StartingPostgres import *
+#
+# bot = telebot.TeleBot(TOKEN)
+# def schedule_checker():
+#     while True:
+#         schedule.run_pending()
+#         sleep(1)
+#
+# def waiting():
+#     # await asyncio.sleep(5)
+#     while True:
+#         transaction_list = list(Sessions.query(TransactionExchange.transaction_id, TransactionExchange.user_id, TransactionExchange.datetime, TransactionExchange.state_transaction).filter(TransactionExchange.state_transaction == 'waiting_user_photo').group_by(TransactionExchange.transaction_id).order_by(TransactionExchange.transaction_id.desc()))
+#         for row in transaction_list:
+#             try:
+#                 datet = datetime.strptime(row[2].strftime("%Y-%m-%d %H:%M:%S"), "%Y-%m-%d %H:%M:%S") + timedelta(minutes=limit_time_send_photo)
+#                 if datet <= datetime.now():
+#                     print("send message")
+#
+#                 else:
+#                     continue
+#
+#             except AttributeError as ex:
+#                 continue
+#
+#
+# def bot_start(bot):
+#
+#     @bot.message_handler(commands=["start"])
+#     def choose_transaction(message):
+#         bot.send_message(message.chat.id, message.text)
+#
+#
+#
+# if __name__ == '__main__':
+#     bot_start(bot)
+#     schedule.every().saturday.at("18:28").do(waiting)
+#     bot.polling(none_stop=True, interval=0)
+# # main_loop = asyncio.get_event_loop()
+# # cors = asyncio.wait([waiting(), bot_start(bot)])
+# # main_loop.run_until_complete(cors)
+# # main_loop.run_forever()
+#
+#
+#
+#
+# # .strftime("%m/%d/%Y, %H:%M:%S")
--- con/classes/SQL/tables/conf/config.py
+++ con/classes/SQL/tables/conf/config.py
@@ -2,7 +2,6 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-# from con.classes.conf.configuration import *
 from sqlalchemy.ext.declarative import declarative_base
 
 load_dotenv()
@@ -10,6 +9,7 @@ load_dotenv()
 Base = declarative_base()
 
 # SQLALCHEMY_DATABASE_URI = f"postgres://{os.environ['NAME']}:{os.environ['PASSWORD']}@{os.environ['HOST']}:{os.environ['PORT']}/{os.environ['DATABASE']}"
+
 SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
