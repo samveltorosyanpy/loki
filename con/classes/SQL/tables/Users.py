bb14f84beaf51d666149fa79390ee0e2a1d8c490
Samvel <samveltorosyan90@gmail.com>
2022-09-26 16:57:21 +0400

    change the token in url, it's string 3

--- con/classes/SQL/tables/Users.py
+++ con/classes/SQL/tables/Users.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, String
-from con.classes.SQL.tables.conf.config import Base, Session
+from conf.config import Base, Session
 from loguru import logger
 
 Sessions = Session()
